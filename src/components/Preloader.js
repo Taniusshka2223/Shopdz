5373067e5f6c0254a10bedc187c16ef2ed3e4690
Taniusshka2223 <135761346+Taniusshka2223@users.noreply.github.com>
2024-04-12 11:11:23 +0300

    first commit

---
+++ src/components/Preloader.js
@@ -0,0 +1,7 @@
+export default function Preloader() {
+    return (
+        <div className="progress">
+            <div className="indeterminate purple accent-1"></div>
+        </div>
+    );
+}
---
+++ src/components/ShopCard.js
@@ -0,0 +1,28 @@
+export default function ShopCard(props) {
+    const {
+        id,
+        name,
+        price,
+        images,
+        appendToCart
+    } = props;
+    const item = {id: id, name: name, price: price}
+    return (
+        <div id={"product-" + id} className="card">
+            <div className="card-image waves-effect waves-block waves-light">
+                <img className="activator" src={images.icon} alt="" />
+            </div>
+            <div className="card-content">
+                <span className="card-title activator grey-text text-darken-4">
+                    {name}
+                </span>
+                <p>Цена: {price} руб.</p>
+            </div>
+            <div className="card-action">
+                <button className="btn-small purple accent-4" onClick={() => appendToCart(item, 1)} >
+                    Купить
+                </button>
+            </div>
+        </div>
+    );
+}
